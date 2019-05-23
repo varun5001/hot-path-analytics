8aa95cdabefd5727faa745cdaf6777b960f7c4c5
Sriram <vsriram7@dxc.com>
2019-05-22 20:12:28 -0500

    config.py updated

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+RESOURCE = os.environ['RESOURCE']  
+TENANT = os.environ['TENANT']  
+AUTHORITY_HOST_URL = os.environ['AUTHORITY_HOST_URL']
+CLIENT_ID = os.environ['CLIENT_ID']  #  Application ID of  app from  Azure portal
+CLIENT_SECRET = os.environ['CLIENT_SECRET']  # value of key  generated when setting up the application
+CONNECTION_STRING = os.environ['CONNECTION_STRING']
+# These settings are for the Microsoft Graph API Call
+API_VERSION = os.environ['API_VERSION']
